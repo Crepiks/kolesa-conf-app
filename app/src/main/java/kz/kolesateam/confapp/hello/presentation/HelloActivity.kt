96c23fac685aecda7e3f9cd9655c96548320e0d9
Sayazhan Onglassyn <sayazhan.onlassyn@mail.ru>
2020-10-31 17:41:12 +0600

    feat: enable continue button when text is entered

--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/HelloActivity.kt
@@ -2,8 +2,11 @@ package kz.kolesateam.confapp.hello.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import kz.kolesateam.confapp.R
 
 private const val TAG = "HelloActivity"
@@ -12,5 +15,19 @@ class HelloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
+
+        val nameEditText = findViewById<EditText>(R.id.activity_hello_name_input)
+        val continueButton = findViewById<Button>(R.id.activity_hello_continue_button)
+
+        nameEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val isInputEmpty: Boolean = s.toString().isBlank()
+                continueButton.isEnabled = !isInputEmpty
+            }
+        })
     }
 }
