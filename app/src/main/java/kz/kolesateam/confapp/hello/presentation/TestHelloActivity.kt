5ca4e52e71e20e1947f49c81f6f97800797424c7
Sayazhan Onglassyn <sayazhan.onlassyn@mail.ru>
2020-11-01 21:14:05 +0600

    fix: replace shared pref mode constant with context value

--- app/src/main/java/kz/kolesateam/confapp/hello/presentation/TestHelloActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/hello/presentation/TestHelloActivity.kt
@@ -1,12 +1,12 @@
 package kz.kolesateam.confapp.hello.presentation
 
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kz.kolesateam.confapp.R
 
-private const val PRIVATE_MODE = 0
 private const val PREFERENCE_NAME = "user-name"
 
 class TestHelloActivity : AppCompatActivity() {
@@ -14,7 +14,7 @@ class TestHelloActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_hello)
 
-        val sharedPref: SharedPreferences = getSharedPreferences(PREFERENCE_NAME, PRIVATE_MODE)
+        val sharedPref: SharedPreferences = getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
         val userName: String = sharedPref.getString(PREFERENCE_NAME, "").toString()
         val nameTextView = findViewById<TextView>(R.id.activity_test_hello_greeting_text)
         nameTextView.text = resources.getString(R.string.greeting, userName)
