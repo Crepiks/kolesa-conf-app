a4caea916dd024642e876410feba8431eb1e0349
Crepiks <sayazhan.onlassyn@mail.ru>
2020-11-24 00:16:56 +0600

    fix: remove markup from events screen

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/UpcomingEventsActivity.kt
@@ -2,11 +2,7 @@ package kz.kolesateam.confapp.events.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.annotation.ColorRes
-import androidx.core.content.ContextCompat
 import kz.kolesateam.confapp.R
 import kz.kolesateam.confapp.events.data.models.BranchApiData
 import kz.kolesateam.confapp.events.data.UpcomingEventsApiClient
@@ -25,99 +21,33 @@ val apiClient: UpcomingEventsApiClient = apiRetrofit.create(UpcomingEventsApiCli
 
 class UpcomingEventsActivity : AppCompatActivity() {
 
-    private lateinit var loadDataResultTextView: TextView
     private lateinit var progressBar: ProgressBar
-    private lateinit var loadSyncButton: Button
-    private lateinit var loadAsyncButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upcoming_events)
 
-        loadDataResultTextView = findViewById(R.id.activity_upcoming_events_request_result)
         progressBar = findViewById(R.id.activity_upcoming_events_progress_bar)
-
-        loadSyncButton = findViewById(R.id.activity_upcoming_events_sync_loading_button)
-        loadSyncButton.setOnClickListener {
-            loadSyncData()
-        }
-
-        loadAsyncButton = findViewById(R.id.activity_upcoming_events_async_loading_button)
-        loadAsyncButton.setOnClickListener {
-            loadAsyncData()
-        }
     }
 
-    private fun loadAsyncData() {
+    private fun fetchData() {
         startLoading()
         apiClient.getUpcomingEvents().enqueue(object: Callback<List<BranchApiData>> {
             override fun onResponse(call: Call<List<BranchApiData>>, response: Response<List<BranchApiData>>) {
                 finishLoading()
-                handleSuccessResponse(response, R.color.activity_upcoming_events_async_text_color)
             }
 
             override fun onFailure(call: Call<List<BranchApiData>>, t: Throwable) {
                 finishLoading()
-                handleFailureResponse(t)
             }
         })
     }
 
-    private fun loadSyncData() {
-        startLoading()
-        Thread {
-            try {
-                val response: Response<List<BranchApiData>> = apiClient.getUpcomingEvents().execute()
-                runOnUiThread {
-                    handleSuccessResponse(response, R.color.activity_upcoming_events_sync_text_color)
-                }
-            } catch(e: Exception) {
-                runOnUiThread {
-                    handleFailureResponse(e)
-                }
-            }
-            runOnUiThread {
-                finishLoading()
-            }
-        }.start()
-    }
-
-    private fun handleSuccessResponse(
-        response: Response<List<BranchApiData>>,
-        @ColorRes successColor: Int = R.color.activity_upcoming_events_error_text_color
-    ) {
-        if (response.isSuccessful) {
-            val branchesList: List<BranchApiData> = response.body()!!
-            changeText(branchesList.toString(), successColor)
-        } else {
-            val errorMessage: String = response.errorBody().toString()
-            changeText(errorMessage, R.color.activity_upcoming_events_error_text_color)
-        }
-    }
-
-    private fun handleFailureResponse(
-        throwable: Throwable
-    ) {
-        val errorMessage = throwable.localizedMessage
-        changeText(errorMessage, R.color.activity_upcoming_events_error_text_color)
-    }
-
     private fun startLoading() {
-        loadSyncButton.gone()
-        loadAsyncButton.gone()
-        loadDataResultTextView.gone()
         progressBar.show()
     }
 
     private fun finishLoading() {
-        loadSyncButton.show()
-        loadAsyncButton.show()
-        loadDataResultTextView.show()
         progressBar.gone()
-    }
-
-    private fun changeText(text: String, color: Int) {
-        loadDataResultTextView.text = text
-        loadDataResultTextView.setTextColor(ContextCompat.getColor(this, color))
     }
 }
