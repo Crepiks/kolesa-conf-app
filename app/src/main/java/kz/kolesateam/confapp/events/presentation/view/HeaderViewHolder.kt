653f2a616bccba48016e9c2c0e42b967943e0749
Crepiks <sayazhan.onlassyn@mail.ru>
2020-11-27 20:26:50 +0600

    fix: move greeting format text into resources

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/view/HeaderViewHolder.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/view/HeaderViewHolder.kt
@@ -5,15 +5,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kz.kolesateam.confapp.R
 
-const val GREETING_FORMAT = "Привет, %s"
-
-class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class HeaderViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
     private val greeting: TextView = view.findViewById(R.id.item_upcoming_event_header_greeting)
 
     fun bind(userName: String) {
         if (userName.isNotEmpty()) {
-            greeting.text = GREETING_FORMAT.format(userName)
+            greeting.text = view.context.getString(R.string.item_upcoming_events_header_greeting_fmt, userName)
         }
     }
 }
