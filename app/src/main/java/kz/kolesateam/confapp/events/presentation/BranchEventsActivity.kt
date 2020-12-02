0ae85fb687f9dda4a0e770e2ba2ef5d1e4c20d4a
Crepiks <sayazhan.onlassyn@mail.ru>
2020-12-02 10:52:19 +0600

    feat: create branch event screen's header

--- app/src/main/java/kz/kolesateam/confapp/events/presentation/BranchEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/events/presentation/BranchEventsActivity.kt
@@ -1,12 +1,30 @@
 package kz.kolesateam.confapp.events.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kz.kolesateam.confapp.R
 
 class BranchEventsActivity : AppCompatActivity() {
+
+    private lateinit var backArrow: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_branch_events)
+        bindViews()
+    }
+
+    private fun bindViews() {
+        backArrow = findViewById(R.id.activity_branch_events_back_arrow)
+        backArrow.setOnClickListener {
+            handleBackArrowClick()
+        }
+    }
+
+    private fun handleBackArrowClick() {
+        val intent = Intent(this, UpcomingEventsActivity::class.java)
+        startActivity(intent)
     }
 }
