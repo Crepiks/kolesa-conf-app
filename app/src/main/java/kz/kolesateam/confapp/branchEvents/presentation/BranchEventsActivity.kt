06dccb2bf2155be4a43542d128b0bb621d5d2287
Crepiks <sayazhan.onlassyn@mail.ru>
2020-12-08 23:27:23 +0600

    refactor: move generation of branch events list into view model

--- app/src/main/java/kz/kolesateam/confapp/branchEvents/presentation/BranchEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/branchEvents/presentation/BranchEventsActivity.kt
@@ -1,8 +1,6 @@
 package kz.kolesateam.confapp.branchEvents.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -10,28 +8,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kz.kolesateam.confapp.R
-import kz.kolesateam.confapp.branchEvents.data.models.EventApiData
 import kz.kolesateam.confapp.branchEvents.presentation.models.BranchEventListItem
-import kz.kolesateam.confapp.branchEvents.presentation.models.EventItem
-import kz.kolesateam.confapp.branchEvents.presentation.models.HeaderItem
 import kz.kolesateam.confapp.branchEvents.presentation.view.EventListAdapter
 import kz.kolesateam.confapp.common.models.ProgressStatus
-import kz.kolesateam.confapp.events.presentation.UpcomingEventsActivity
 import kz.kolesateam.confapp.extension.gone
 import kz.kolesateam.confapp.extension.show
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-private const val TAG = "BranchEventsActivity"
 private const val DEFAULT_BRANCH_ID = 0
 private const val DEFAULT_BRANCH_TITLE = ""
+private const val BRANCH_ID_KEY = "BRANCH_ID"
+private const val BRANCH_TITLE_KEY = "BRANCH_TITLE"
 
 class BranchEventsActivity : AppCompatActivity() {
 
     private val branchEventsViewModel: BranchEventsViewModel by viewModel()
     private val eventListAdapter = EventListAdapter()
 
-    private var branchId: Int = DEFAULT_BRANCH_ID
-    private lateinit var branchTitle: String
     private lateinit var backArrow: View
     private lateinit var eventListRecyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
@@ -40,27 +33,27 @@ class BranchEventsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_branch_events)
 
-        branchId = getBranchId()
-        branchTitle = getBranchTitle()
-
         bindViews()
 
-        branchEventsViewModel.onStart(branchId = branchId)
-        observerBranchEventsViewModel()
+        observerBranchEventsLiveData()
+        branchEventsViewModel.onStart(
+            branchId = getBranchId(),
+            branchTitle = getBranchTitle()
+        )
     }
 
     private fun getBranchId(): Int {
-        return intent.getIntExtra("BRANCH_ID", DEFAULT_BRANCH_ID)
+        return intent.getIntExtra(BRANCH_ID_KEY, DEFAULT_BRANCH_ID)
     }
 
     private fun getBranchTitle(): String {
-        val branchTitle = intent.getStringExtra("BRANCH_TITLE");
+        val branchTitle = intent.getStringExtra(BRANCH_TITLE_KEY);
         return branchTitle ?: DEFAULT_BRANCH_TITLE
     }
 
-    private fun observerBranchEventsViewModel() {
+    private fun observerBranchEventsLiveData() {
         branchEventsViewModel.getProgressLiveData().observe(this, ::handleLoadingStatusChange)
-        branchEventsViewModel.getBranchListLiveData().observe(this, ::showEventList)
+        branchEventsViewModel.getBranchEventListLiveData().observe(this, ::showBranchEventList)
         branchEventsViewModel.getErrorLiveData().observe(this, ::showErrorMessage)
     }
 
@@ -76,31 +69,21 @@ class BranchEventsActivity : AppCompatActivity() {
     }
 
     private fun handleBackArrowClick() {
-        val intent = Intent(this, UpcomingEventsActivity::class.java)
-        startActivity(intent)
+        finish()
     }
 
     private fun handleLoadingStatusChange(status: ProgressStatus) {
         when (status) {
             ProgressStatus.Loading -> progressBar.show()
-            else -> progressBar.gone()
+            ProgressStatus.Finished -> progressBar.gone()
         }
     }
 
-    private fun showEventList(eventList: List<EventApiData>) {
-        val branchEventList = getBranchEventList(eventList)
+    private fun showBranchEventList(branchEventList: List<BranchEventListItem>) {
         eventListAdapter.setList(branchEventList)
     }
 
     private fun showErrorMessage(errorMessage: String) {
         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun getBranchEventList(eventList: List<EventApiData>): List<BranchEventListItem> {
-        val headerItem = HeaderItem(title = branchTitle)
-        val eventItemList = eventList.map { event ->
-            EventItem(data = event)
-        }
-        return listOf(headerItem) + eventItemList
     }
 }
--- app/src/main/java/kz/kolesateam/confapp/branchEvents/presentation/BranchEventsViewModel.kt
+++ app/src/main/java/kz/kolesateam/confapp/branchEvents/presentation/BranchEventsViewModel.kt
@@ -7,31 +7,38 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kz.kolesateam.confapp.branchEvents.data.BranchEventsDataSource
 import kz.kolesateam.confapp.branchEvents.data.models.EventApiData
 import kz.kolesateam.confapp.branchEvents.domain.BranchEventsRepository
+import kz.kolesateam.confapp.branchEvents.presentation.models.BranchEventListItem
+import kz.kolesateam.confapp.branchEvents.presentation.models.EventItem
+import kz.kolesateam.confapp.branchEvents.presentation.models.HeaderItem
 import kz.kolesateam.confapp.common.models.ProgressStatus
 import kz.kolesateam.confapp.common.models.ResponseData
-import retrofit2.Response
 
 private const val DEFAULT_BRANCH_ID = 0
+private const val DEFAULT_BRANCH_TITLE = ""
 
 class BranchEventsViewModel(
     private val branchEventsRepository: BranchEventsRepository
 ) : ViewModel() {
 
     private val progressLiveData: MutableLiveData<ProgressStatus> = MutableLiveData()
-    private val branchListLiveData: MutableLiveData<List<EventApiData>> = MutableLiveData()
+    private val branchEventsListLiveData: MutableLiveData<List<BranchEventListItem>> = MutableLiveData()
     private val errorLiveData: MutableLiveData<String> = MutableLiveData()
 
     fun getProgressLiveData(): LiveData<ProgressStatus> = progressLiveData
-    fun getBranchListLiveData(): LiveData<List<EventApiData>> = branchListLiveData
+    fun getBranchEventListLiveData(): LiveData<List<BranchEventListItem>> = branchEventsListLiveData
     fun getErrorLiveData(): LiveData<String> = errorLiveData
 
     private var branchId = DEFAULT_BRANCH_ID
+    private var branchTitle = DEFAULT_BRANCH_TITLE
 
-    fun onStart(branchId: Int) {
+    fun onStart(
+        branchId: Int,
+        branchTitle: String
+    ) {
         this.branchId = branchId
+        this.branchTitle = branchTitle
         fetchData()
     }
 
@@ -45,11 +52,19 @@ class BranchEventsViewModel(
             when (response) {
                 is ResponseData.Success -> {
                     val branchList = response.result
-                    branchListLiveData.value = branchList
+                    branchEventsListLiveData.value = getBranchEventList(branchList)
                 }
                 is ResponseData.Error -> errorLiveData.value = response.error
             }
             progressLiveData.value = ProgressStatus.Finished
         }
+    }
+
+    private fun getBranchEventList(eventList: List<EventApiData>): List<BranchEventListItem> {
+        val headerItem = HeaderItem(title = branchTitle)
+        val eventItemList = eventList.map { event ->
+            EventItem(data = event)
+        }
+        return listOf(headerItem) + eventItemList
     }
 }
