cdb1b61dec3a160c66f0cac4e9d524caaaf7b5d9
Crepiks <sayazhan.onlassyn@mail.ru>
2020-12-21 00:30:46 +0600

    feat: display empty placeholder when favorites list is empty

--- app/src/main/java/kz/kolesateam/confapp/favorites/presentation/FavoritesActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/favorites/presentation/FavoritesActivity.kt
@@ -2,6 +2,7 @@ package kz.kolesateam.confapp.favorites.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import kz.kolesateam.confapp.R
 import kz.kolesateam.confapp.common.models.EventData
 import kz.kolesateam.confapp.eventDetails.presentation.EventDetailsRouter
+import kz.kolesateam.confapp.extension.gone
+import kz.kolesateam.confapp.extension.show
 import kz.kolesateam.confapp.favorites.presentation.view.EventListAdapter
 import kz.kolesateam.confapp.upcomingEvents.presentation.UpcomingEventsRouter
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -23,6 +26,7 @@ class FavoritesActivity : AppCompatActivity() {
 
     private lateinit var eventListRecyclerView: RecyclerView
     private lateinit var homeButton: Button
+    private lateinit var emptyContainer: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +48,8 @@ class FavoritesActivity : AppCompatActivity() {
         eventListRecyclerView.adapter = eventListAdapter
         eventListRecyclerView.layoutManager = LinearLayoutManager(this)
 
+        emptyContainer = findViewById(R.id.activity_favorites_empty_container)
+
         homeButton = findViewById(R.id.activity_favorites_home_button)
         homeButton.setOnClickListener {
             navigateToHomeScreen()
@@ -51,7 +57,7 @@ class FavoritesActivity : AppCompatActivity() {
     }
 
     private fun observeFavoritesLiveData() {
-        favoritesViewModel.getFavoritesLiveData().observe(this, ::setEventList)
+        favoritesViewModel.getFavoritesLiveData().observe(this, ::handleFavoritesChange)
     }
 
     private fun handleFavoriteClick(event: EventData) {
@@ -69,6 +75,17 @@ class FavoritesActivity : AppCompatActivity() {
     private fun navigateToHomeScreen() {
         val upcomingEventsIntent: Intent = UpcomingEventsRouter().createIntent(this)
         startActivity(upcomingEventsIntent)
+    }
+
+    private fun handleFavoritesChange(eventList: List<EventData>) {
+        if (eventList.isEmpty()) {
+            emptyContainer.show()
+            eventListRecyclerView.gone()
+        } else {
+            emptyContainer.gone()
+            eventListRecyclerView.show()
+            setEventList(eventList)
+        }
     }
 
     private fun setEventList(eventList: List<EventData>) {
